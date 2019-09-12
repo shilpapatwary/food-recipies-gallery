968a87f24c2729904ad3c5ec540789574394457f
Shilpa Patwary <shilpa.patwary@sapient.com>
2019-09-12 16:18:45 +0530

    Refactoring

--- food-recipies-ui/src/sagas/apis.ts
+++ food-recipies-ui/src/sagas/apis.ts
@@ -6,12 +6,12 @@ const client = contentful.createClient({
   environment: 'master', 
   accessToken: '7ac531648a1b5e1dab6c18b0979f822a5aad0fe5f1109829b8a197eb2be4b84c'
 })
-
+const entryConfig = {
+  content_type: 'recipe',
+  select: 'sys.id,fields.title,fields.photo,fields.tags,fields.description,fields.chef'
+};
 export async function getRecipesContent() {
-     const response = await client.getEntries({
-        content_type: 'recipe',
-        select: 'sys.id,fields.title,fields.photo,fields.tags,fields.description,fields.chef'
-     });
+     const response = await client.getEntries(entryConfig);
      const body = await response;
      if (response.errors) throw Error('error');
      return {
@@ -22,12 +22,8 @@ export async function getRecipesContent() {
 }
 
 export async function getRecipeContent(id: String) {
-  const response = await client.getEntry(id, {
-    content_type: 'recipe',
-    select: 'sys.id,fields.title,fields.photo,fields.tags,fields.description,fields.chef'
-  });
+  const response = await client.getEntry(id, entryConfig);
   const body = await response;
-  console.log(body);
   if (response.errors) throw Error('error');
   return body;
 }
